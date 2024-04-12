6a1b45df1ec4c3892d55d1d94719a921f4df41c2
Meelis Utt <meelis.utt@gmail.com>
2024-04-13 00:12:49 +0200

    minor tweaks

--- 2015d4_v2.c
+++ 2015d4_v2.c
@@ -26,28 +26,6 @@ BYTE PADDING[64] = {
 	0, 0, 0, 0
 };
 
-WORD table[64] = {
-	0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
-	0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
-	0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
-	0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
-
-	0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
-	0xd62f105d,	0x02441453,	0xd8a1e681,	0xe7d3fbc8,
-	0x21e1cde6,	0xc33707d6,	0xf4d50d87,	0x455a14ed,
-	0xa9e3e905,	0xfcefa3f8,	0x676f02d9,	0x8d2a4c8a,
-
-	0xfffa3942,	0x8771f681,	0x6d9d6122,	0xfde5380c,
-	0xa4beea44,	0x4bdecfa9,	0xf6bb4b60,	0xbebfbc70,
-	0x289b7ec6,	0xeaa127fa,	0xd4ef3085,	0x04881d05,
-	0xd9d4d039,	0xe6db99e5,	0x1fa27cf8,	0xc4ac5665,
-
-	0xf4292244,	0x432aff97,	0xab9423a7,	0xfc93a039,
-	0x655b59c3,	0x8f0ccc92,	0xffeff47d,	0x85845dd1,
-	0x6fa87e4f,	0xfe2ce6e0,	0xa3014314,	0x4e0811a1,
-	0xf7537e82,	0xbd3af235,	0x2ad7d2bb,	0xeb86d391
-};
-
 #define S11 7
 #define S12 12
 #define S13 17
@@ -102,10 +80,10 @@ void my_memset(unsigned char *dest, unsigned char c, size_t len) {
 void strbits(unsigned char *b, size_t len) {
 	unsigned long long int lenb = len << 3;
 	for (int i=0; i<2; ++i) {
-		b[4*i+0] = (unsigned char)((lenb >> ( 0+(32*i))) & 0xff);
-		b[4*i+1] = (unsigned char)((lenb >> ( 8+(32*i))) & 0xff);
-		b[4*i+2] = (unsigned char)((lenb >> (16+(32*i))) & 0xff);
-		b[4*i+3] = (unsigned char)((lenb >> (24+(32*i))) & 0xff);
+		b[4*i+0] = (unsigned char)(lenb >> ( 0+(32*i)));
+		b[4*i+1] = (unsigned char)(lenb >> ( 8+(32*i)));
+		b[4*i+2] = (unsigned char)(lenb >> (16+(32*i)));
+		b[4*i+3] = (unsigned char)(lenb >> (24+(32*i)));
 	}
 }
 
@@ -144,35 +122,19 @@ WORD aux_i(WORD x, WORD y, WORD z) {
 }
 
 WORD round_1(WORD a, WORD b, WORD c, WORD d, WORD x_k, WORD t_i, int s) {
-	WORD result;
-	result = a + aux_f(b, c, d) + x_k + t_i;
-	result = rot_left(result, s);
-	result += b;
-	return result;
+	return b + rot_left(a + aux_f(b, c, d) + x_k + t_i, s);
 }
 
 WORD round_2(WORD a, WORD b, WORD c, WORD d, WORD x_k, WORD t_i, int s) {
-	WORD result;
-	result = a + aux_g(b, c, d) + x_k + t_i;
-	result = rot_left(result, s);
-	result += b;
-	return result;
+	return b + rot_left(a + aux_g(b, c, d) + x_k + t_i, s);
 }
 
 WORD round_3(WORD a, WORD b, WORD c, WORD d, WORD x_k, WORD t_i, int s) {
-	WORD result;
-	result = a + aux_h(b, c, d) + x_k + t_i;
-	result = rot_left(result, s);
-	result += b;
-	return result;
+	return b + rot_left(a + aux_h(b, c, d) + x_k + t_i, s);
 }
 
 WORD round_4(WORD a, WORD b, WORD c, WORD d, WORD x_k, WORD t_i, int s) {
-	WORD result;
-	result = a + aux_i(b, c, d) + x_k + t_i;
-	result = rot_left(result, s);
-	result += b;
-	return result;
+	return b + rot_left(a + aux_i(b, c, d) + x_k + t_i, s);
 }
 
 void printer(unsigned char *digest, size_t len) {
@@ -209,7 +171,9 @@ void md5(char *m) {
 		WORD x[16];
 		my_memset((unsigned char *)x, 0, 16);
 		for (int j=0; j<16; ++j) {
-			x[j] = message_pp[i*16+j];
+			unsigned char tmp = message_pp[i*16+j];
+			x[j] = tmp >> (8*0) | tmp >> (8*1) | tmp >> (8*2) | tmp >> (8*3);
+			// x[j] = message_pp[i*16+j];
 		}
 
 		WORD a = state[0];
@@ -296,6 +260,6 @@ void md5(char *m) {
 }
 
 int main() {
-	md5("abcdef609043");
 	md5("1234567890");
+	md5("abcdef609043");
 }
