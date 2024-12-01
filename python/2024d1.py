9027636d9d5117e88c70c610a7698f2953385eb5
utt <meelis.utt@gmail.com>
2024-12-01 22:36:51 +0100

    python - 2024d1 minor improvement

--- python/2024d1.py
+++ python/2024d1.py
@@ -1,23 +1,23 @@
 
 def ex1(filename):
 	with open(filename, "r") as f:
-		lines = f.readlines()
+		lines = f.read().split("\n")
 
-	a = sorted([int(line.split("   ")[0].strip("\n")) for line in lines])
-	b = sorted([int(line.split("   ")[1].strip("\n")) for line in lines])
+	a = sorted([int(line.split("   ")[0]) for line in lines])
+	b = sorted([int(line.split("   ")[1]) for line in lines])
 	dist = [abs(a[i]-b[i]) for i in range(0, len(a))]
 	return sum(dist)
 
 def ex2(filename):
 	with open(filename, "r") as f:
-		lines = f.readlines()
+		lines = f.read().split("\n")
 
 	mp = {}
 	for line in lines:
-		idx = int(line.split("   ")[1].strip("\n"))
+		idx = int(line.split("   ")[1])
 		mp[idx] = int(mp.get(idx) or 0) + 1
 
-	a = sorted([int(line.split("   ")[0].strip("\n")) for line in lines])
+	a = sorted([int(line.split("   ")[0]) for line in lines])
 	scores = [ai * int(mp.get(ai) or 0) for ai in a]
 	return sum(scores)
 
