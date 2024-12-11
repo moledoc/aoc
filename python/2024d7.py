1d59b3fcef22f4f1c08fc985d3ee7f0b58da03a1
utt <meelis.utt@gmail.com>
2024-12-11 16:24:52 +0100

    python: 2024d7

--- python/2024d7.py
+++ python/2024d7.py
@@ -2,38 +2,42 @@
 def intify(str_list):
 	return [int(elem) for elem in str_list]
 
-def plus(a, b):
-	return a + b
+def plus(a, b, rest):
+	return max(a + b, b)
 
-def mult(a, b):
-	return a * b
+def mult(a, b, rest):
+	return max(a * b, b)
 
-# debug = []
-
-def ex1_recur(target, acc, op_funcs, rest):
-	# global debug
+def ex_recur(target, acc, op_funcs, rest):
 	if len(rest) == 0:
-		return 0
+		return acc if acc == target else 0
 	for op_func in op_funcs:
-		# debug.append({rest[0], op_func})
-		# print(debug)
-		acc1 = op_func(acc, rest[0])
-		if target == acc1:
-			return target
-		if target < acc1:
-			# debug = debug[0:-1]
+		rest1 = rest[:]
+		acc1 = op_func(acc, rest[0], rest1)
+		if acc1 > target:
 			continue
-		r = ex1_recur(target, acc1, op_funcs, rest[1::])
+		r = ex_recur(target, acc1, op_funcs, rest1[1::])
 		if r != 0:
 			return r
-		# debug = debug[0:-1]
-	# debug = debug[0:-1]
 	return 0
 
 def ex1(filename):
 	with open(filename, "r") as f:
 		lines = [intify(line.replace(":", "").split(" ")) for line in f.read().strip("\n").split("\n")]
-	return sum([ex1_recur(line[0], line[1], [mult, plus], line[2::]) for line in lines])
+	return sum([ex_recur(line[0], line[1], [mult, plus], line[2::]) for line in lines])
+
+def concat(a, b, rest):
+	if len(rest) > 0:
+		a = int(str(a) + str(rest[0]))
+		rest = rest[1:]
+	return a
+
+# NOTE: a bit slow, but beareble
+def ex2(filename):
+	with open(filename, "r") as f:
+		lines = [intify(line.replace(":", "").split(" ")) for line in f.read().strip("\n").split("\n")]
+	return sum([ex_recur(line[0], line[1], [mult, plus, concat], line[2::]) for line in lines])
 
 print(f"ex1: {ex1("./input.txt")}")
-# print(f"ex2: {ex2("./sample.txt")}")
+print(f"ex2: {ex2("./input.txt")}")
+
