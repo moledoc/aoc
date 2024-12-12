b142961894765f9ac43d28471af07906337c2952
utt <meelis.utt@gmail.com>
2024-12-12 23:21:17 +0100

    python: 2024d09e2 - current solution, but its incorrect atm

--- python/2024d09.py
+++ python/2024d09.py
@@ -1,9 +1,12 @@
+def pprint(int_list):
+	
+	print("".join([str(x) for x in int_list if x != -1]))
+
 
 def ex1(filename):
 	with open(filename, "r") as f:
 		disk_map = [int(line) for line in f.read().strip("\n")]
 
-	# print(len(disk_map))
 	disk_map_len = len(disk_map)
 
 	free_spaces = [0 for _ in range(disk_map_len//2)]
@@ -19,8 +22,6 @@ def ex1(filename):
 		else:
 			free_spaces[s_idx] = disk_map[i]
 			s_idx += 1
-	# print(file_blocks)
-	# print(free_spaces)
 
 	new_disk_mapping = []
 
@@ -35,7 +36,7 @@ def ex1(filename):
 
 		# handle free space by consuming free space from beginning
 		# and file blocks at the end
-		# MAYBE: TODO: currently consuming file blocks one at a time, but maybe should do it smarter
+		# NOTE: fit block-by-block
 		for fre in range(free_spaces[0]):
 			if len(file_blocks) == 0 or len(free_spaces) == 0:
 				break
@@ -47,12 +48,75 @@ def ex1(filename):
 				file_blocks = file_blocks[:len(file_blocks)-1]
 		free_spaces = free_spaces[1::]
 
-	# print("".join([str(x) for x in new_disk_mapping]))
+	# pprint(new_disk_mapping)
 	checksum = 0
 	for i in range(len(new_disk_mapping)):
 		checksum += i * new_disk_mapping[i]
 	return checksum
 
+# 6787922826883 too high
+def ex2(filename):
+	with open(filename, "r") as f:
+		disk_map = [int(line) for line in f.read().strip("\n")]
+
+	disk_map_tuples = [(-1, -1) for _ in range(len(disk_map))]
+
+	id = 0
+	for i in range(len(disk_map)):
+		if i % 2 == 0:
+			disk_map_tuples[i] = (id, disk_map[i])
+			id += 1
+		else:
+			disk_map_tuples[i] = (-1, disk_map[i])
+
+	r_offset = len(disk_map_tuples)
+
+	seen = {}
+	while True:
+		r_offset -= 1
+		if r_offset < 0:
+			break
+
+		r_id, r_count = disk_map_tuples[r_offset]
+
+		if r_id == -1:
+			continue
+
+		for l in range(len(disk_map_tuples)):
+			l_id, l_count = disk_map_tuples[l]
+			if l_id != -1:
+				continue
+			if l > r_offset: # don't move stuff we already touched
+				break
+			if r_count == l_count: # swap places
+				disk_map_tuples[l] = (r_id, r_count)
+				disk_map_tuples[r_offset] = (-1, r_count)
+				r_offset -= 1
+				break
+			elif r_count < l_count:
+				# swap empty with file block
+				disk_map_tuples[l] = (l_id, l_count - r_count)
+				disk_map_tuples[r_offset] = (-1, r_count)
+				# add new file block tuple
+				disk_map_tuples.insert(l, (r_id, r_count))
+				r_offset -= 1
+				break
+
+	# print(disk_map_tuples)
+	new_disk_map = []
+	for id, count in disk_map_tuples:
+		new_disk_map.extend([id for _ in range(count)])
+	# pprint(new_disk_map)
+
+	checksum = 0
+	for i in range(len(new_disk_map)):
+		if new_disk_map[i] == -1:
+			continue
+		checksum += i * new_disk_map[i]
+
+	return checksum
+
+
 print(f"ex1: {ex1("./input.txt")}")
-# print(f"ex2: {ex2("./sample.txt")}")
+print(f"ex2: {ex2("./input.txt")}")
 
